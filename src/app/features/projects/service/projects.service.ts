f5febe8d00f54cddec79fa4274493dca9cdbb108
Pashong <suzikaamv@gmail.com>
2026-09-04 17:15:17 +0200

    added project creation overlay

--- src/app/features/projects/service/projects.service.ts
+++ src/app/features/projects/service/projects.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@angular/core';
-import { ProjectModel } from '../model/project.model';
+import { Injectable, signal } from '@angular/core';
+import { environment } from '../../../../environments/environments';
+import { NgForm } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectsService {
+  newProject = signal<boolean>(false);
+  projectId = signal<number>(0);
+
   async getProjects() {
     try {
-      const response = await fetch('http://localhost:3030/projects');
+      const response = await fetch(`${environment.apiUrl}/projects`);
 
       if (!response.ok) {
         throw new Error(`Reponse status: ${response.status}`);
@@ -17,6 +21,25 @@ export class ProjectsService {
       }
     } catch (error) {
       console.log('Fetching projects failed', error);
+    }
+  }
+
+  async createProject(form: NgForm) {
+    try {
+      const response = await fetch(`${environment.apiUrl}/projects/create-project`, {
+        credentials: 'include',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(form.value),
+      });
+
+      if (!response.ok) {
+        throw new Error('Reponse status creation: ' + response.status);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error('Something went wrong creating your project', error);
     }
   }
 }
