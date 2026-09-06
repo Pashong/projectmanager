f1c211e79f8c2477a9440bd876693fa71d63a9d0
Pashong <suzikaamv@gmail.com>
2026-09-06 20:24:54 +0200

    added a delete-task component

---
+++ src/app/features/tasks/components/delete-task/delete-task.ts
@@ -0,0 +1,27 @@
+import { Component, inject, input } from '@angular/core';
+import { TaskService } from '../../service/task.service';
+import { TaskModel } from '../../model/task.model';
+
+@Component({
+  selector: 'app-delete-task',
+  imports: [],
+  templateUrl: './delete-task.html',
+  styleUrl: './delete-task.scss',
+})
+export class DeleteTask {
+  private tasksService = inject(TaskService);
+  tasks = this.tasksService.tasks;
+
+  task = input.required<TaskModel>();
+
+  async deleteTask(currentTask: TaskModel) {
+    try {
+      await this.tasksService.deleteTask(currentTask);
+      this.tasks.update((tasks) =>
+        tasks.filter((task) => task.id !== currentTask.id),
+      );
+    } catch (error) {
+      console.error('Task deletion error: ', error);
+    }
+  }
+}
