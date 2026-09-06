2e07a9ec1f16b0a7fa5bdc0267b7babbcddf3994
Pashong <suzikaamv@gmail.com>
2026-09-06 22:48:06 +0200

    updated guard that user who are not logged in always get redirected to login route

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,9 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../features/auth/services/auth.service';
 import { inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = async (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
+  const isAuthenticated = await authService.checkAuth();
+  if(isAuthenticated){
+    return true;
+  }
 
-  return authService.checkAuth();
+  return router.createUrlTree(['/login']);
 };
